1458995aaffe18ded28b2163916f8109870b5431
Slava Shpitalny <slavik57@gmail.com>
2018-07-05 14:48:46 +0300

    waitUntilHealthy: Add logs

--- src/waitUntilHealthy.ts
+++ src/waitUntilHealthy.ts
@@ -10,6 +10,7 @@ export interface WaitUntilHealthyConfig extends DockerComposeConfigBase {
 }
 
 export async function waitUntilHealthy(config: WaitUntilHealthyConfig): Promise<boolean> {
+  console.log('waitUntilHealthy: Getting service id')
   const serviceId: string = await getServiceId({
     composeFiles: config.composeFiles,
     cwd: config.cwd,
@@ -17,6 +18,8 @@ export async function waitUntilHealthy(config: WaitUntilHealthyConfig): Promise<
     serviceName: config.serviceName,
     projectName: config.projectName
   });
+
+  console.log(`waitUntilHealthy: Got service id: ${serviceId}`);
 
   const options: ExecOptions = {
     env: config.environmentVariables,
@@ -27,24 +30,34 @@ export async function waitUntilHealthy(config: WaitUntilHealthyConfig): Promise<
   const checkInterval: number = config.checkIntervalMilliseconds || 1000;
   const command = `docker ps --filter id=${serviceId} --format "{{.Status}}"`;
 
+
   let result: CommandResult;
   do {
+    console.log(`waitUntilHealthy: Giving the container time to spin up. Sleeping for [${checkInterval}] milliseconds.`)
+    await new Promise((resolve) => setTimeout(resolve, checkInterval));
+
+    console.log(`waitUntilHealthy: Checking container state using the command [${command}]. Number of retries left: ${retries}. CheckIntervalMilliseconds: ${checkInterval}`);
     result = await executeCommand(command, options);
+    console.log('waitUntilHealthy: Command result', result);
+
     if (result.error) {
-      console.log(result.stderr);
+      console.log('waitUntilHealthy: Failed checking the container status. Error:', result.stderr);
       throw result.error;
     }
 
     retries--;
     if (retries < 0) {
+      console.log('waitUntilHealthy: No more retries left, reporting container as unhealthy')
       return false;
     }
-    await new Promise((resolve) => setTimeout(resolve, checkInterval));
   } while (!result.stdout || result.stdout.indexOf('starting') >= 0);
 
   if (result.stdout.indexOf('unhealthy') >= 0) {
+    console.log('waitUntilHealthy: Container is reporting an unhealthy state')
     return false;
   }
+
+  console.log('waitUntilHealthy: Container is up. Reporing container as healthy')
 
   return true;
 }
